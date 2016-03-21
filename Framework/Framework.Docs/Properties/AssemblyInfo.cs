160d8e23f7d173877325f31d187e8777c6e2d21b
Joao Paulo Carreiro <joaopaulocarreiro@gmail.com>
2016-03-21 09:50:28 -0100

    docs

--- Framework/Framework.Docs/Properties/AssemblyInfo.cs
+++ Framework/Framework.Docs/Properties/AssemblyInfo.cs
@@ -1,43 +1,27 @@
-﻿using Microsoft.Owin;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-//
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-//
-
-[assembly: AssemblyTitle("FW.SingleAPP1")]
+[assembly: AssemblyTitle("Framework.Docs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Cybermap")]
-[assembly: AssemblyProduct("FW.SingleAPP1")]
-[assembly: AssemblyCopyright("Copyright © Cybermap 2014")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Framework.Docs")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-//
-// Owin application startup class.
-//
-
-[assembly: OwinStartup(typeof(Framework.Configuration.Startup))]
-
-//
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-//
-
 [assembly: ComVisible(false)]
 
-// 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-//
+[assembly: Guid("8dee43b5-4063-4f9d-974a-c368c8294178")]
 
-[assembly: Guid("3c7cc323-c796-4a97-a6e8-a0c9c90123d0")]
-
-//
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -45,9 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-//
-
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
