c616efd249740f504eb6d91989466adbd584691b
Joao Paulo Carreiro <joaopaulocarreiro@gmail.com>
2016-03-28 18:10:26 +0000

    client

---
+++ Framework/Framework.Klowd/Client/fw/modules/fw/error.js
@@ -0,0 +1,56 @@
+﻿// ============================================================================
+// Project: Framework
+// Name/Class: Core module. String service.
+// Created On: 28/Mar/2016
+// Author: João Carreiro (joao.carreiro@cybermap.pt)
+// Company: Cybermap Lda.
+// ============================================================================
+
+'use strict';
+fw.module('fw').service('error', 'fw.util', function ($util) {
+
+    var _errMsg = function (lib, bag, descriptor, error) {
+
+        //
+        // Base message.
+        //
+
+        var msg = '[' + lib + ']:' + bag[descriptor];
+
+        //
+        // Instantiate aditional parameters, placeholder: {argN}
+        //
+
+        if (arguments.length > 4) {
+
+            for (var i = 4, j = 0; i < arguments.length; i++, j++) {
+
+                msg = msg.replace('{arg' + j + '}', arguments[i]);
+            }
+        }
+
+        //
+        // If caller is defined, call it here.
+        //
+
+        if ($util.isDefined(error)) {
+            error(msg);
+        }
+
+        //
+        // dump to console.
+        //
+
+        console.error(msg);
+
+        return false;
+    };
+
+    //
+    // API
+    //
+
+    return {
+
+    };
+});
