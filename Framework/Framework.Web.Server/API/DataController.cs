843db959c78ccefe4e84e81f2640be8528cbd7b9
Joao Paulo Carreiro <joaopaulocarreiro@gmail.com>
2016-06-09 12:36:35 +0000

    web.server

---
+++ Framework/Framework.Web.Server/API/DataController.cs
@@ -0,0 +1,13 @@
+﻿// ============================================================================
+// Project: Framework
+// Name/Class: 
+// Author: João Carreiro (joao.carreiro@cybermap.pt)
+// Create date: 04/Oct/2015
+// Company: Cybermap Lta.
+// Description:
+// ============================================================================
+
+namespace Framework.Web.Server.API
+{
+    public class DataController : Data.API.ADataController { }
+}
