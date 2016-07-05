02f3644b156f270296cc1a46567bf9d3fae2a6d3
Joao Paulo Carreiro <joaopaulocarreiro@gmail.com>
2016-07-05 12:59:00 +0000

    all

---
+++ Framework/Framework.Auth/Model/Config/LibConfiguration.cs
@@ -0,0 +1,17 @@
+﻿// ============================================================================
+// Project: Framework
+// Name/Class: 
+// Author: João Carreiro (joao.carreiro@cybermap.pt)
+// Create date: 5/Jul/2016
+// Company: Coop4Creativity
+// Description: Configuration objects.
+// ============================================================================
+
+using System.Configuration;
+
+namespace Framework.Auth.Model.Config
+{
+    public class LibConfiguration : ConfigurationSection
+    {       
+    }
+}
