33c5dffef55745078be1b27b8ab497409a66b238
Joao Paulo Carreiro <joaopaulocarreiro@gmail.com>
2016-07-05 22:50:55 +0000

    all

---
+++ Framework/Framework.Apps/Model/Objects/App.cs
@@ -0,0 +1,70 @@
+﻿// ============================================================================
+// Project: Framework
+// Name/Class: 
+// Author: João Carreiro (joaopaulocarreiro@gmail.com)
+// Create date: 04/Oct/2015
+// Company: Coop4Creativity
+// Description:
+// ============================================================================
+
+using System;
+using System.Collections.Generic;
+using Framework.Core.Patterns;
+
+namespace Framework.Apps.Model.Objects
+{
+    public class App :
+        IID<int>,
+        IVisible,
+        ICreated<string>,
+        IModified<string>,
+        IRef<string>,
+        IName<string>,
+        IDescription<string>
+    {
+        //
+        // PROPERTIES
+        //
+
+        public int ID { get; set; }
+        public TypeOfVisibility Visibility { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public string Ref { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+       
+
+        //
+        // CONSTRUCTORS
+        //
+
+        public App()
+        {
+            //
+            // Base
+            //
+
+            ID = -1;
+            Visibility = TypeOfVisibility.ACTIVE;
+
+            DateTime dateNow = DateTime.Now;
+            CreatedDate = new DateTime(dateNow.Ticks);
+            ModifiedDate = new DateTime(dateNow.Ticks);
+            CreatedBy = string.Empty;
+            ModifiedBy = string.Empty;
+
+            //
+            // Info.
+            //
+
+            Ref = string.Empty;
+            Name = string.Empty;
+            Description = string.Empty;         
+        }
+    }
+}
---
+++ Framework/Framework.Factory/API/SrvEventHub.cs
@@ -0,0 +1,22 @@
+﻿// ============================================================================
+// Project: Framework
+// Name/Class: SrvReflected
+// Author: João Carreiro (joaopaulocarreiro@gmail.com)
+// Create date: 08/Mar/2016
+// Company: Coop4Creativity
+// Description: Reflected service implementation.
+// ============================================================================
+
+using Framework.Core.Extensions;
+using Framework.Factory.Patterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Framework.Factory.API
+{
+    public class SrvEventHub : ACommon, IEventHub
+    {
+    }
+}
