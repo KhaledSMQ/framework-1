1b5deedcbf743f07c14c8a35c7bdb53b2fdeb984
Joao Paulo Carreiro <joaopaulocarreiro@gmail.com>
2016-10-06 22:10:55 +0000

    runtime;

---
+++ Framework/Framework.Packages/Services/SrvBundles.cs
@@ -0,0 +1,27 @@
+﻿// ============================================================================
+// Project: Framework
+// Name/Class: 
+// Author: João Carreiro (joaopaulocarreiro@gmail.com)
+// Create date: 25/Mar/2016
+// Company: Coop4Creativity
+// Description: 
+// ============================================================================
+
+using Framework.Core.Patterns;
+using Framework.Packages.Api;
+
+namespace Framework.Packages.Services
+{
+    public class SrvBundles : ACommon, IBundles
+    {
+        //
+        // Get the bundle content.
+        //
+
+        public string GetBundleContent(string names, string mimeType, TypeOfContentFormat minify, object ctx)
+        {
+            string content = string.Empty;
+            return content;
+        }
+    }
+}
