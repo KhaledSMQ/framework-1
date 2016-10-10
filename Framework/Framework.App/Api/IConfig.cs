db2390bfc1ecb9539ef2e950b60fa438af4e1891
Joao Paulo Carreiro <joaopaulocarreiro@gmail.com>
2016-10-10 17:42:50 +0000

    app;

---
+++ Framework/Framework.App/Api/IConfig.cs
@@ -0,0 +1,20 @@
+﻿// ============================================================================
+// Project: Framework
+// Name/Class: IConfig
+// Author: João Carreiro (joao.carreiro@coop4creativity.com)
+// Create date: 26/Nov/2015
+// Company: Coop4Creativity
+// Description: Configuration service for application management,
+// ============================================================================
+
+using Framework.Core.Api;
+
+namespace Framework.App.Api
+{
+    public interface IConfig : ICommon
+    {
+        void Load();
+
+        void Reload();
+    }
+}
---
+++ Framework/Framework.App/Services/Config.cs
@@ -0,0 +1,33 @@
+﻿// ============================================================================
+// Project: Framework
+// Name/Class: Config
+// Author: João Carreiro (joao.carreiro@coop4creativity.com)
+// Create date: 26/Nov/2015
+// Company: Coop4Creativity
+// Description: Scope service implementation..
+// ============================================================================
+
+using Framework.App.Api;
+using Framework.Core.Patterns;
+
+namespace Framework.App.Services
+{
+    public class Config : ACommon, IConfig
+    {
+        //
+        // PROPERTIES
+        //
+
+        //
+        // API
+        //
+
+        public void Load()
+        {
+        }
+
+        public void Reload()
+        {
+        }
+    }
+}
